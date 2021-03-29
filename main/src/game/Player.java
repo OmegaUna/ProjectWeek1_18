b98f4a03eee648b88343d04f1932fba329d68753
Klaas <klaas.neyens@gmail.com>
2021-03-29 11:03:21 +0200

    Created Player class

--- main/src/game/Player.java
+++ main/src/game/Player.java
@@ -1,4 +1,37 @@
 package game;
 
 public class Player {
+
+    private String name;
+    private int score;
+
+    public void Player(String name) {
+        this.name = name;
+        this.score = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public void addToScore(int add) {
+        this.score += add;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + " " + this.score;
+    }
 }
