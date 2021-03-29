bf113dcfa724db2a8302788bb77f3fc8b64335a2
Klaas <klaas.neyens@gmail.com>
2021-03-29 20:08:34 +0200

    Point compareTo

--- main/src/shapes/Point.java
+++ main/src/shapes/Point.java
@@ -22,6 +22,10 @@ public class Point {
         return this.getX() == point.getX() && this.getY() == point.getY();
     }
 
+    public int compareTo(Point point) {
+        return this.getX() - point.getX();
+    }
+
     @Override
     public String toString() {
         return String.format("(%s %s)", this.getX(), this.getY());
