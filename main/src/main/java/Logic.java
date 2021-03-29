bdd2563b0df4b4b3619af4079422a5cc5402695e
Dazzy <maxys3@hotmail.com>
2021-03-29 10:28:02 +0200

    Updated game logic

--- main/src/main/java/Logic.java
+++ main/src/main/java/Logic.java
@@ -4,13 +4,35 @@ public class Logic {
 
 
     private ArrayList<String> usedLetters = new ArrayList<>();
-    private String wordAnswer;
+    private String wordAnswer = "";
+    private int guessedCount = 0;
+    private int max_guesses = 0;
+
+
 
     public void addUsedLetter(String letter) {
-        usedLetters.add(letter);
+        this.usedLetters.add(letter);
+        this.guessedCount++;
     }
+    /*  Button:onclick => checkLetter */
     public boolean checkLetter(String letter) {
-        return this.usedLetters.get(0).equals(letter); // random placeholder
+        this.addUsedLetter(letter);
+        // onderste lijn moet misschien op een andere plaats staan wegens LOGIC!
+        if (guessedCount == max_guesses) this.loseGame();
+        return wordAnswer.contains(letter) && !this.usedLetters.contains(letter);
+        /* if word has been guessed eventually */
+    }
+
+
+
+
+    /*   Change text to output => winGame */
+    public void winGame() {
+        System.out.println("You have won the game!");
+    }
+    /*   Change text to output => loseGame */
+    public void loseGame() {
+        System.out.println("You have losed the game!");
     }
 
 }
