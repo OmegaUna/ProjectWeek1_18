87d1b30c0753ae8f40f5bc49936323581fb56070
LarsPoortmansUCLL <78728714+LarsPoortmansUCLL@users.noreply.github.com>
2021-03-30 11:48:58 +0200

    moved boxaround to package shapes

--- Test/domain/AllTests.java
+++ Test/domain/AllTests.java
@@ -1,14 +1,9 @@
 package domain;
 import domain.Game.SpelerTest;
-import domain.shapes.CircleTest;
-import domain.shapes.LijnStuk;
-import domain.shapes.LijnstukTest;
-import domain.shapes.PuntTest;
+import domain.shapes.*;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
-
-import java.util.function.BinaryOperator;
 
 @RunWith(Suite.class)
 @SuiteClasses({ PuntTest.class, SpelerTest.class, CircleTest.class, LijnstukTest.class, BoxAroundTest.class})
--- Test/domain/shapes/BoxAroundTest.java
+++ Test/domain/shapes/BoxAroundTest.java
@@ -1,4 +1,4 @@
-package domain;
+package domain.shapes;
 
 import static org.junit.Assert.*;
 
