a3e751242ba8a37f7af2c3f4129782ff20591e14
LarsPoortmansUCLL <78728714+LarsPoortmansUCLL@users.noreply.github.com>
2021-03-30 11:28:44 +0200

    Update AllTests.java

--- Test/domain/AllTests.java
+++ Test/domain/AllTests.java
@@ -1,13 +1,15 @@
 package domain;
 import domain.Game.SpelerTest;
 import domain.shapes.CircleTest;
+import domain.shapes.LijnStuk;
+import domain.shapes.LijnstukTest;
 import domain.shapes.PuntTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ PuntTest.class, SpelerTest.class, CircleTest.class})
+@SuiteClasses({ PuntTest.class, SpelerTest.class, CircleTest.class, LijnstukTest.class})
 public class AllTests {
 
 }
